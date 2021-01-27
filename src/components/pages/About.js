243a93d93c53b1920d28851ab279f3847b6139da
Khawar Qureshi <khawarqureshi08@gmail.com>
2021-01-27 15:19:27 +0500

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return(
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the Todo App. And it is developed by using react js.</p>     
+        </React.Fragment>
+    )
+ }
+ 
+ export default About;
